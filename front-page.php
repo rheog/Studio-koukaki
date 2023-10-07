d33ddd54f64ec7673552962d20d68b625555cec4
rheog <135063724+rheog@users.noreply.github.com>
2023-10-07 19:15:01 +0200

    Add files via upload

--- front-page.php
+++ front-page.php
@@ -4,7 +4,10 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-    <section class="banner fade-in">
+    <section class="banner fade-in parallax">
+        <video autoplay muted loop>
+            <source src="<?php echo get_stylesheet_directory_uri(); ?>/images/studiovideo.mp4" type="video/mp4">
+        </video>
         <img src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
 
     </section>
@@ -26,27 +29,7 @@ get_header();
         <article id="characters">
             <div class="main-character">
                 <h3>Les personnages</h3>
-                <?php
-                $main_character = $characters_query->posts[0];
-                echo '<figure>';
-                echo get_the_post_thumbnail($main_character->ID, 'full');
-                echo '<figcaption>' . $main_character->post_title . '</figcaption>';
-                echo '</figure>';
-                $characters_query->next_post();
-                ?>
-            </div>
-            <div class="other-characters">
-                <?php
-                while ($characters_query->have_posts()) {
-                    $characters_query->the_post();
-                    echo '<figure>';
-                    echo get_the_post_thumbnail(get_the_ID(), 'full');
-                    echo '<figcaption>';
-                    the_title();
-                    echo '</figcaption>';
-                    echo '</figure>';
-                }
-                ?>
+                <?php get_template_part('coverflow'); ?>
             </div>
         </article>
         <article id="place">
@@ -71,17 +54,9 @@ get_header();
             <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
         </div>
     </section>
-    <section id="oscars" class="in-view">
-        <div class="container">
-            <h3>Fleurs d’oranger & chats errants est nominé aux Oscars Short Film Animated de 2022 !</h3>
-        </div>
-        <div class="oscars_img">
-        </div>
-    </section>
+
 </main><!-- #main -->
 
 <?php
 get_footer();
 ?>
-<!-- Inclusion du script JavaScript personnalisé -->
-<script src="<?php echo get_template_directory_uri() . 'script.js'; ?>"></script>
